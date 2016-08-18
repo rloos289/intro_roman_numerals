d24c1d3fd78019b9a385bc4878c742f637edbad7
Jason Downing and Ryan Loos <jason@>
2016-08-18 11:24:02 -0700

    add function that filters exceptions, deleted console logs

--- js/scripts.js
+++ js/scripts.js
@@ -13,47 +13,63 @@ function translate (number) {
   if (newNumber >= 1000) {
     newNumber -= 1000;
     symbols.push('M');
-    console.log(newNumber);
     moreThanZero()
     return newNumber;
   } else if (newNumber >= 500) {
     newNumber -= 500;
     symbols.push('D');
-    console.log(newNumber);
     moreThanZero()
     return newNumber;
   } else if (newNumber >= 100) {
     newNumber -= 100;
     symbols.push('C');
-    console.log(newNumber);
     moreThanZero()
     return newNumber;
   } else if (newNumber >= 50) {
     newNumber -= 50;
     symbols.push('L');
-    console.log(newNumber);
     moreThanZero()
     return newNumber;
   } else if (newNumber >= 10) {
     newNumber -= 10;
     symbols.push('X');
-    console.log(newNumber);
     moreThanZero()
     return newNumber;
   } else if (newNumber >= 5) {
     newNumber -= 5;
     symbols.push('V');
-    console.log(newNumber);
     moreThanZero()
     return newNumber;
   } else if (newNumber >= 1) {
     newNumber -= 1;
     symbols.push('I');
-    console.log(newNumber);
     moreThanZero()
     return newNumber;
   }
 
+}
+
+function exceptions() {
+  var string = symbols.join('');
+  var dcSearch = /DCCCC/;
+  var cSearch = /CCCC/;
+  var lxSearch = /LXXXX/;
+  var xSearch = /XXXX/;
+  var viSearch =/VIIII/;
+  var iSearch = /IIII/;
+  if (dcSearch.test(string)) {
+    string = string.replace('DCCCC','CM');
+  } if (cSearch.test(string)) {
+    string = string.replace('CCCC','CD');
+  } if (lxSearch.test(string)) {
+    string = string.replace('LXXXX','XC');
+  } if (xSearch.test(string)) {
+    string = string.replace('XXXX','XL');
+  } if (viSearch.test(string)) {
+    string = string.replace('VIIII','IX');
+  } if (iSearch.test(string)) {
+    string = string.replace('IIII','IV');
+  } return string;
 }
 
 //<!-- Front End  -->
@@ -62,5 +78,6 @@ $(document).ready(function(){
     event.preventDefault();
     var userInput = $('#userNumber').val();
     romanNum(userInput);
+    // dont let user put in more than 3999
   });
 });
