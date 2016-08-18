bb8bfcc4ff5ed47ce416154fc08de5b773ded6a7
Jason Downing and Ryan Loos <ryan@>
2016-08-18 09:42:23 -0700

    add function that subracts from original number and logs subtraction as a symbol in an array

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,41 @@
 //<!-- Back End -->
+var symbols = [];
+var newNumber = '';
+
+function moreThanZero() {
+  if (newNumber > 0) {
+    translate(newNumber);
+  }
+}
+
+function translate (number) {
+  newNumber = number;
+  if (newNumber >= 1000) {
+    newNumber -= 1000;
+    symbols.push('M');
+    console.log(newNumber);
+    moreThanZero()
+    return newNumber;
+  } else if (newNumber >= 500) {
+    newNumber -= 500;
+    symbols.push('D');
+    console.log(newNumber);
+    moreThanZero()
+    return newNumber;
+  } else if (newNumber >= 100) {
+    newNumber -= 100;
+    symbols.push('C');
+    console.log(newNumber);
+    moreThanZero()
+    return newNumber;
+}
+}
 
 //<!-- Front End  -->
 $(document).ready(function(){
   $('form').submit(function(event) {
     event.preventDefault();
     var userInput = $('#userNumber').val();
-    console.log(userInput);
+    romanNum(userInput);
   });
 });
